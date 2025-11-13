1ae42edddeb1c1e481434b1ea9be9443b95f5311
Agus Gunawan <140695049+guzgnwn@users.noreply.github.com>
2025-11-14 02:46:27 +0700

    Remove menu toggle functionality from main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -210,9 +210,3 @@
 
 })();
 
-
-const menu=document.querySelector(".menu");
-const toggle=document.querySelector(".toggle");
-toggle.addEventListener("click",()=>{
-  menu.classList.toggle("active");
-})
